81051b66f3e37b220cc18c9f88cd46385a1f5d39
JimmyChenX <130384737+JimmyChenX@users.noreply.github.com>
2026-08-05 23:51:08 +0800

    Update download.sh

--- tools/download.sh
+++ tools/download.sh
@@ -35,7 +35,7 @@ wget -nv "https://ci.android.com/builds/submitted/15885347/aosp_cf_x86_64_only_p
 if [ "$TARGET_ARCH" = "$HOST_ARCH" ]; then
     FILE_NAME="cvd-host_package.tar.gz"
     echo "-> arch matched: (run $TARGET_ARCH on $HOST_ARCH)), downloading $FILE_NAME..."
-    wget -nv ${RURL_CI_ANDROID%/view/BUILD_INFO}/raw/cvd-host_package.tar.gz -O cvd-host_package.tar.gz
+    wget -nv "https://ci.android.com/builds/submitted/15885347/aosp_cf_x86_64_only_phone-userdebug/latest/raw/cvd-host_package.tar.gz" -O cvd-host_package.tar.gz
 elif [ "$TARGET_ARCH" != "x86_64" ] && [ "$HOST_ARCH" = "x86_64" ]; then
     FILE_NAME="cvd-host_package-x86_64.tar.gz"
     echo "->  cross arch detected:  (run $TARGET_ARCH on $HOST_ARCH), downloading $FILE_NAME..."
