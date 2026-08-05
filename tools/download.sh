9c246e6738d04d27f684d90909cadf446423f094
JimmyChenX <130384737+JimmyChenX@users.noreply.github.com>
2026-08-05 23:47:38 +0800

    Update download.sh

--- tools/download.sh
+++ tools/download.sh
@@ -30,7 +30,7 @@ RURL_CI_ANDROID=$(curl -Ls -o /dev/null -w %{url_effective} ${URL_CI_ANDROID})
 VERSION=${RURL_CI_ANDROID%/$TARGET/latest/view/BUILD_INFO}
 VERSION=${VERSION#https://ci.android.com/builds/submitted/}
 
-wget -nv ${RURL_CI_ANDROID%/view/BUILD_INFO}/raw/${DEVICE}-img-$VERSION.zip -O ${DEVICE}-img.zip
+wget -nv "https://ci.android.com/builds/submitted/15885347/aosp_cf_x86_64_only_phone-userdebug/latest/raw/aosp_cf_x86_64_only_phone-img-15885347.zip" -O ${DEVICE}-img.zip
 
 if [ "$TARGET_ARCH" = "$HOST_ARCH" ]; then
     FILE_NAME="cvd-host_package.tar.gz"
@@ -39,7 +39,7 @@ if [ "$TARGET_ARCH" = "$HOST_ARCH" ]; then
 elif [ "$TARGET_ARCH" != "x86_64" ] && [ "$HOST_ARCH" = "x86_64" ]; then
     FILE_NAME="cvd-host_package-x86_64.tar.gz"
     echo "->  cross arch detected:  (run $TARGET_ARCH on $HOST_ARCH), downloading $FILE_NAME..."
-    wget -nv ${RURL_CI_ANDROID%/view/BUILD_INFO}/raw/cvd-host_package-x86_64.tar.gz -O cvd-host_package.tar.gz
+    wget -nv "https://ci.android.com/builds/submitted/15885347/aosp_cf_x86_64_only_phone-userdebug/latest/raw/cvd-host_package.tar.gz" -O cvd-host_package.tar.gz
 else
     echo "-> arch not matched: (not support run $TARGET_ARCH on $HOST_ARCH)"
     exit 1
